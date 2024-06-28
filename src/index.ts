a1e68b5be5165b66700511b7854ac30d24190597
Yuta Suzuki <yutasuzuki.r@gmail.com>
2024-06-28 16:45:36 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import RecordScreen from 'react-native-record-screen';
+import type { RecordingResponse } from 'react-native-record-screen';
+export default RecordScreen;
+export type { RecordingResponse };
